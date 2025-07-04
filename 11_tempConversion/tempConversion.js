80b32a27eb665ef54ddef9332d8745208575c8b5
kroma2 <janickijakub05@gmail.com>
2025-07-04 17:21:34 +0200

    do tasks 6-11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(celcTemp) {
+   celcTemp = (celcTemp - 32) * 5/9
+   return parseFloat(celcTemp.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrTemp) {
+  fahrTemp = fahrTemp * 9/5 + 32
+  return parseFloat(fahrTemp.toFixed(1))
 };
 
 // Do not edit below this line
